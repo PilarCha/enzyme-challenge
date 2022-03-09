f654395a2d1f0a6bd8d971f7fdd4e4bf7905f052
Pilar Chavez <pchavez92@gmail.com>
2022-03-08 18:43:35 -0800

    setting up the routes page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  get 'distance/index'
+  root to: 'distance#index'
+
+  get '/search' => 'distance#search'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
