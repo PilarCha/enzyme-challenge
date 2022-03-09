120b84d724517cc9029ecd4f77d7932d0ab125ce
Pilar Chavez <pchavez92@gmail.com>
2022-03-08 18:47:44 -0800

    adding a flash notice if no food trucks are found for the search criteria

--- app/controllers/distance_controller.rb
+++ app/controllers/distance_controller.rb
@@ -1,4 +1,12 @@
 class DistanceController < ApplicationController
-  def index    
+  def index
+
   end
+
+  def search
+    food_trucks = find_trucks(params[:keyword])
+
+    unless food_trucks
+      flash[:alert] = 'No food trucks were found'
+      return render action: :index
 end
