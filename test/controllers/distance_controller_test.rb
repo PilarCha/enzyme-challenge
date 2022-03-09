b23e2df9a37dd657d5f1685127b597d200b8755e
Pilar Chavez <pchavez92@gmail.com>
2022-03-08 18:39:20 -0800

    created controller and 1 view page

---
+++ test/controllers/distance_controller_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class DistanceControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get distance_index_url
+    assert_response :success
+  end
+
+end
